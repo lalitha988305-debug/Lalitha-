0572a8d348162afc9bca48230aea161b8f88e39f
lalitha988305-debug <lalitha988305@gmail.com>
2025-11-07 10:49:23 +0530

    Add files via upload

---
+++ Area of circle.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{ 
+  float radius,area;
+  printf("enter radius");
+  scanf("%f",&radius);
+  area=3.14*radius*radius;
+  printf("\n enter the area of circle:%.2f",area);
+  
+  
+	return 0;
+}
